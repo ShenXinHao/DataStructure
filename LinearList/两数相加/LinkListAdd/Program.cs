42f802581048c9969406774fbe31dd21588dfc33
ShenXinHao <1571988545@qq.com>
2024-11-01 01:33:18 +0800

    两数相加

---
+++ LinearList/两数相加/LinkListAdd/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class ListNode {
+     public int val;
+     public ListNode next;
+     public ListNode(int val = 0, ListNode next = null) {
+             this.val = val;
+             this.next = next;
+        }
+}
+
+
+internal class Program
+{
+    static ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode();
+        
+        var result = temp;
+        while(list1 != null && list2 != null)
+        {
+            
+                if(list1.val <= list2.val)
+                {
+                    result.next = list1;
+                   
+                    list1 = list1.next;
+                    
+                }
+                else
+                {
+                    result.next = list2;
+                    list2 = list2.next;
+                }
+                result = result.next;
+        }
+        result.next = list1 == null ? list2 : list1;
+        return temp.next;
+    }
+    
+    public static void Main(string[] args)
+    {
+        //var list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        //var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        var list1 = new ListNode(2);
+        var list2 = new ListNode(1);
+        MergeTwoLists(list1, list2);
+    }
+}
