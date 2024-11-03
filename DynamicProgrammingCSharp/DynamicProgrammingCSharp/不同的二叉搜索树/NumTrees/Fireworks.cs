e612355949e9b053dec7f252138779503efc074d
ShenXinHao <1571988545@qq.com>
2024-11-04 00:47:35 +0800

    不同的二叉搜索树

---
+++ DynamicProgrammingCSharp/DynamicProgrammingCSharp/不同的二叉搜索树/NumTrees/Fireworks.cs
@@ -0,0 +1,70 @@
+﻿namespace NumTrees;
+
+public class Fireworks
+{
+    static Random random = new Random();
+
+ 
+    public static void LaunchFirework()
+    {
+        int launchX = random.Next(10, Console.WindowWidth - 10); // 随机发射位置
+        int launchHeight = random.Next(Console.WindowHeight / 2, Console.WindowHeight - 5); // 随机高度
+
+        // 烟花上升效果
+        for (int y = Console.WindowHeight - 2; y > launchHeight; y--)
+        {
+            Console.SetCursorPosition(launchX, y);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("|"); // 烟花上升轨迹
+            Thread.Sleep(50);
+
+            // 清除上一个位置的轨迹
+            Console.SetCursorPosition(launchX, y);
+            Console.Write(" ");
+        }
+
+        // 烟花爆炸效果
+        Explode(launchX, launchHeight);
+    }
+
+    public static void Explode(int centerX, int centerY)
+    {
+        int explosionSize = random.Next(5, 10); // 爆炸的范围大小
+
+        // 烟花的爆炸效果
+        for (int i = 0; i < explosionSize * 2; i++)
+        {
+            int offsetX = random.Next(-explosionSize, explosionSize);
+            int offsetY = random.Next(-explosionSize, explosionSize);
+
+            int x = centerX + offsetX;
+            int y = centerY + offsetY;
+
+            if (x >= 0 && x < Console.WindowWidth && y >= 0 && y < Console.WindowHeight)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = (ConsoleColor)random.Next(1, 16); // 随机颜色
+                Console.Write("*"); // 烟花的火花字符
+            }
+        }
+
+        Thread.Sleep(500); // 保持爆炸效果一会儿
+
+        // 清除爆炸效果
+        for (int i = 0; i < explosionSize * 2; i++)
+        {
+            int offsetX = random.Next(-explosionSize, explosionSize);
+            int offsetY = random.Next(-explosionSize, explosionSize);
+
+            int x = centerX + offsetX;
+            int y = centerY + offsetY;
+
+            if (x >= 0 && x < Console.WindowWidth && y >= 0 && y < Console.WindowHeight)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write(" ");
+            }
+        }
+        Console.ResetColor();
+    }
+}
---
+++ DynamicProgrammingCSharp/DynamicProgrammingCSharp/不同的二叉搜索树/NumTrees/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+using NumTrees;
+
+/*
+ 
+ 二叉搜索树是一个有序树：
+
+若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值；
+若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值；
+它的左、右子树也分别为二叉搜索树
+这就决定了，二叉搜索树，递归遍历和迭代遍历和普通二叉树都不一样。
+ 
+ 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+
+
+输入：n = 3
+输出：5
+
+输入：n = 1
+输出：1
+
+
+1.确定dp[i]含义
+    dp[i] 为n时总共有多少种二叉搜索树
+2.递推公式
+    首先是对于dp[3]来说，dp[1]和dp[2]可以组成dp[3],
+    对于不重复的二叉搜索树来说，左子结点必定小于当前结点，右子节点必须大于当前结点
+    所以对于dp[3]来说，有左0右2，有左1右1，有左1右0这三种情况
+    左右互相乘积再相加就是dp[3]
+    dp[i] = sum(dp[0]*dp[i] + dp[1]*dp[i-1].....)
+3.确定初值
+    dp[0]=1
+    dp[1]=1
+    dp[2]=2
+4.确定从前向后顺序
+    
+5.举例
+dp[3] = dp[0]*dp[2] + dp[1]*dp[1] + dp[2]*dp[0]= 2 + 1 + 2
+
+*/
+
+
+int NumTrees(int n)
+{
+    var dp = new int[n + 1];
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    var sum = 0;
+    for (int i = 3; i <= n; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < i; j++)
+        {
+            sum += dp[j]* dp[i - j - 1];
+        }
+
+        dp[i] = sum;
+    }
+    Console.WriteLine(dp[n - 1]);
+    return dp[n - 1];
+} 
+
+NumTrees(2);
+NumTrees(3);
+NumTrees(4);
+NumTrees(5);
+NumTrees(6);
