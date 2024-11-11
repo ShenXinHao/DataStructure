748aeab5fceb9b6437345abf4bb4cc1cc1d89190
ShenXinHao <1571988545@qq.com>
2024-11-12 02:04:25 +0800

    长度最小子数组

---
+++ 数组/有序数组的平方/Program.cs
@@ -0,0 +1,70 @@
+﻿
+/*
+https://leetcode.cn/problems/squares-of-a-sorted-array/submissions/579855445/
+给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+
+ 
+
+示例 1：
+
+输入：nums = [-4,-1,0,3,10]
+输出：[0,1,9,16,100]
+解释：平方后，数组变为 [16,1,0,9,100]
+排序后，数组变为 [0,1,9,16,100]
+示例 2：
+
+输入：nums = [-7,-3,2,3,11]
+输出：[4,9,9,49,121]
+ 
+
+提示：
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums 已按 非递减顺序 排序
+ 
+
+进阶：
+
+请你设计时间复杂度为 O(n) 的算法解决本问题
+
+
+
+
+
+
+
+
+
+
+*/
+
+int[] SortedSquares(int[] nums)
+{
+    int left = 0;
+    int right = nums.Length - 1;
+    var pos = nums.Length - 1;
+    var newnums = new int[nums.Length];
+    while (left <= right)
+    {
+        if (nums[left] * nums[left] < nums[right] * nums[right])
+        {
+            newnums[pos] = nums[right] * nums[right];
+            right--;
+        }
+        else
+        { 
+            newnums[pos] = nums[left] * nums[left];
+            left++;
+        }
+
+        pos--;
+    }
+    return newnums;
+}
+
+//SortedSquares(new []{-4, -1, 0, 3, 10});
+SortedSquares(new []{-7, -3, 2, 3, 11});
+SortedSquares(new []{-7, -3, -2, -1});
+SortedSquares(new []{-7});
+SortedSquares(new []{7});
---
+++ 数组/长度最小的子数组/Program.cs
@@ -0,0 +1,108 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+/*
+https://leetcode.cn/problems/minimum-size-subarray-sum/
+
+给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+找出该数组中满足其总和大于等于 target 的长度最小的 
+子数组
+ [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+ 
+
+示例 1：
+
+输入：target = 7, nums = [2,3,1,2,4,3]
+输出：2
+解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+示例 2：
+
+输入：target = 4, nums = [1,4,4]
+输出：1
+示例 3：
+
+输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+输出：0
+ 
+
+提示：
+
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+
+进阶：
+
+如果你已经实现 O(n) 时间复杂度的解法, 请尝试设计一个 O(n log(n)) 时间复杂度的解法。
+
+
+*/
+
+int MinSubArrayLen(int target, int[] nums)
+{
+    var sum = 0;
+    var max = 0;
+    var index = 0;
+    bool isleftHeight = false;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        sum += nums[i];
+        if (max < nums[i])
+        {
+            max = nums[i];
+            index = i;
+        }
+        
+    }
+
+    if (sum < target) return 0;
+    if (sum == target) return nums.Length;
+    var left = index - 1 < 0 ? 0 : index - 1 ;
+    var right = index + 1 > nums.Length ? nums.Length - 1 : index + 1;
+    var tempSum = nums[index];
+    while (true)
+    {
+        if (tempSum >= target)
+        {
+            return right - left + 1;
+        }
+        if (tempSum < target && left >= 0)
+        {
+            tempSum += nums[left];
+            left--;
+        }
+      
+        if (tempSum < target && right < nums.Length)
+        {
+            tempSum += nums[right];
+            right++;
+        }
+        if (tempSum >= target)
+        {
+            if (left < 0) left = 0;
+            if(right > nums.Length - 1) right = nums.Length - 1;
+            var count = right - left + 1;
+           
+             if (tempSum - nums[left] - nums[right] >= target)
+            {
+                count = right - left;
+            }
+            else if(tempSum - nums[right] >= target)
+                count = right - left + 1;
+            else if (tempSum - nums[left] >= target)
+                count = right - left - 1;
+            return count;
+        }
+        
+    }
+
+    return 0;
+}
+Console.WriteLine(MinSubArrayLen(7, new []{2,3,1,2,4,3}));
+Console.WriteLine(MinSubArrayLen(4, new []{1,4,4}));
+Console.WriteLine(MinSubArrayLen(15, new []{1,2,3,4,5}));
+Console.WriteLine(MinSubArrayLen(11, new []{1,2,3,4,5}));
+//Console.WriteLine(MinSubArrayLen(11, new []{1,1,1,1,1,1,1,1}));
+Console.WriteLine(MinSubArrayLen(213, new []{12,28,83,4,25,26,25,2,25,25,25,12}));
