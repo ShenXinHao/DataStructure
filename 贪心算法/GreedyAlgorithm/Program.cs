10dc70472941a30a8b42eb1d4223258f872dd4cc
ShenXinHao <65120326+ShenXinHao@users.noreply.github.com>
2024-11-29 23:37:02 +0800

    贪心

---
+++ 贪心算法/GreedyAlgorithm/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+int LongestPalindrome(string s)
+{
+    int count = 0;
+    bool longestPalindrome = false;
+    Dictionary<int, int> results = new Dictionary<int, int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (!results.TryAdd(s[i] - 26, 1))
+        {
+            results[s[i] - 26]++;
+        }
+    }
+
+    foreach (var result in results)
+    {
+        var tempCount = result.Value / 2;
+        var y = result.Value % 2;
+        if (!longestPalindrome && y == 1)
+        {
+            count++;
+            longestPalindrome = true;
+        }
+
+        count += 2 * tempCount;
+    }
+    return count;
+}
